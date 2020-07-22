eb79a452ce022f63fb4c51793f3aab9a68ce4a28
WeeDom <dominic_pain@hotmail.com>
2020-07-22 19:17:18 +0100

    add endpoints, tests and registry

--- reviews/reviews/wsgi.py
+++ reviews/reviews/wsgi.py
@@ -9,30 +9,29 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
+import inspect
+from reviews.ml.registry import MLRegistry
+from reviews.ml.income_classifier.random_forest import RandomForestClassifier
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reviews.settings')
 
 application = get_wsgi_application()
 
-# ML registry
-# import inspect
-# from reviews.ml.registry import MLRegistry
-# from reviews.ml.income_classifier.random_forest import RandomForestClassifier
+try:
+    registry = MLRegistry()
+    #  Random Forest classifier
+    rf = RandomForestClassifier()
+    # add to ML regstry
+    registry.add_algorithm(
+        endpoint_name="income_classifier",
+        algorithm_object=rf,
+        algorithm_name="random forest",
+        algorithm_status="production",
+        algorithm_version="0.0.1",
+        owner="WeeDom",
+        algorithm_description="Random Forest with simple pre- and post-processing",  # noqa 501
+        algorithm_code=inspect.getsource(RandomForestClassifier))
 
-# try:
-    # registry = MLRegistry() # create ML registry
-    # # Random Forest classifier
-    # rf = RandomForestClassifier()
-    # # add to ML registry
-    # registry.add_algorithm(endpoint_name="income_classifier",
-        # algorithm_object=rf,
-        # algorithm_name="random forest",
-        # algorithm_status="production",
-        # algorithm_version="0.0.1",
-        # owner="Piotr",
-        # algorithm_description="Random Forest with simple pre- and post-processing",
-        # algorithm_code=inspect.getsource(RandomForestClassifier))
-
-# except Exception as e:
-    # print("Exception while loading the algorithms to the registry,", str(e))
+except Exception as e:
+    print("Exception while loading the algorithms to the registry,", str(e))
